3939242a53bf16b0a621e9d63cc27b8b4cdc3c7d
Admin <Admin@DESKTOP-6PQE64G>
2024-01-14 13:36:29 +0530

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace loops
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            while(a<=20)
+            {
+                Console.WriteLine("a={0}",a);
+                a++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
